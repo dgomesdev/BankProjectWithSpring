b1550bdd176fb627863371b810ef829f100c8f28
dgomes2602 <danilo.gomes@outlook.fr>
2022-11-20 15:11:53 -0300

    1st test of Bank Project using Spring

--- src/main/java/com/bank/project/BankProjectApplication.java
+++ src/main/java/com/bank/project/BankProjectApplication.java
@@ -3,17 +3,11 @@ package com.bank.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-
 @SpringBootApplication
 public class BankProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankProjectApplication.class, args);
-
-		String name = JOptionPane.showInputDialog("What is your name?");
-		JOptionPane.showMessageDialog(null, "Welcome " + name + "!");
-
-	}
+			}
 
 }
